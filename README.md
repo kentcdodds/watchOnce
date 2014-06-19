dbd2d50c6c3a447078511088acba50da50ff51ed
Kent C. Dodds <kent@doddsfamily.us>
2014-06-18 23:12:27 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# $watchOnce
+
+This module adds `$watchOnce`, `$watchGroupOnce`, and `$watchCollectionOnce` to the `$rootScope`. Each of these have the same signature as their angular counterparts. The difference is the listener you provide is only called once, and only when the value is defined. Note that for `$watchGroupOnce`, it will only be called when every expression's value is defined. Also, `$watchGroupOnce` is only available in Angular >= 1.3. Once your listner has been called, the watcher stop itself from watching.
+
+## To Get
+
+Get the code on [GitHub](http://github.com/kentcdodds/watchOnce): [Scope.$watchOnce.js](https://raw.githubusercontent.com/kentcdodds/watchOnce/master/Scope.$watchOnce.js) | [Scope.$watchOnce.min.js](https://raw.githubusercontent.com/kentcdodds/watchOnce/master/Scope.$watchOnce.min.js)
+
+Or by using bower or npm:
+
+`bower|npm install watchOnce`
+
+## To Use
+
+Include it on your page with a script tag. And add it to your dependencies:
+
+```javascript
+angular.module('app', 'Scope.$wachOnce');
+
+// Then you can do this:
+
+$scope.$watchOnce('expression', function(value) {
+  console.log(value); // <-- will only print once and will never be undefined.
+});
+
+// you can also use $watchGroupOnce and $watchCollectionOnce
+```
+
+See the test-runner.js and the [demo page](http://kent.doddsfamily.us/watchOnce) (which runs the test runner).
+
+## Angular compatability
+
+This is compatible with Angular >= 1.0.0. Note, that older versions only had `$watch` so only `$watchOnce` is supported for the older versions.
+
+## License
+
+MIT
---
+++ test-runner.js
@@ -0,0 +1,107 @@
+angular.module('example', ['Scope.$watchOnce']).run(function($rootScope) {
+  var calls = {
+    $watch: 0,
+    $watchOnce: 0,
+
+    $watchGroup: 0,
+    $watchGroupOnce: 0,
+
+    $watchCollection: 0,
+    $watchCollectionOnce: 0
+  };
+
+  var watchTypes = [];
+  angular.forEach(['$watch', '$watchGroup', '$watchCollection'], function(type) {
+    if (angular.isFunction($rootScope[type])) {
+      watchTypes.push(type);
+    }
+  });
+
+  var expressions = {
+    $watch: 'objs.$watch',
+    $watchGroup: ['objs.$watchGroup1', 'objs.$watchGroup2'],
+    $watchCollection: 'objs.$watchCollection'
+  };
+
+  var firstAssignments = {
+    $watch: '$watch',
+    $watchGroup: ['$watchGroup1', '$watchGroup2'],
+    $watchCollection: ['$watchCollection1', '$watchCollection2']
+  };
+
+  var secondAssignments = {
+    $watch: '$watchAgain',
+    $watchGroup: ['$watchGroup1Again', '$watchGroup2Again'],
+    $watchCollection: ['$watchCollection1Again', '$watchCollection2Again']
+  };
+
+  var finalValues = {};
+
+  $rootScope.testResults = '';
+
+  $rootScope.objs = {
+    $watch: undefined,
+    $watchGroup1: undefined,
+    $watchGroup2: undefined,
+    $watchCollection: undefined
+  };
+
+  iterateTypes(function(type, typeOnce) {
+    $rootScope[type](expressions[type], getIncrementer(type));
+    $rootScope[typeOnce](expressions[type], getIncrementer(typeOnce));
+  });
+
+  setTimeout(function() {
+    $rootScope.objs.$watch = firstAssignments.$watch;
+    $rootScope.objs.$watchGroup1 = firstAssignments.$watchGroup[0];
+    $rootScope.objs.$watchGroup2 = firstAssignments.$watchGroup[1];
+    $rootScope.objs.$watchCollection = firstAssignments.$watchCollection;
+    $rootScope.$apply();
+  }, 1);
+
+  setTimeout(function() {
+    $rootScope.objs.$watch = secondAssignments.$watch;
+    $rootScope.objs.$watchGroup1 = secondAssignments.$watchGroup[0];
+    $rootScope.objs.$watchGroup2 = secondAssignments.$watchGroup[1];
+    $rootScope.objs.$watchCollection = secondAssignments.$watchCollection;
+    $rootScope.$apply();
+  }, 2);
+
+  setTimeout(function() {
+    iterateTypes(function(type) {
+      printResults(type);
+    });
+    $rootScope.$apply();
+  }, 3);
+
+  function printResults(type) {
+    var typeOnce = type + 'Once';
+    var normalWorked = 3 === calls[type] && angular.equals(finalValues[type], secondAssignments[type]);
+    var onceWorked = 1 === calls[typeOnce] && angular.equals(finalValues[typeOnce], firstAssignments[type]);
+    $rootScope.testResults += type + ' Results\n';
+    $rootScope.testResults += getResult(type, 3, normalWorked);
+    $rootScope.testResults += '\n';
+    $rootScope.testResults += getResult(typeOnce, 1, onceWorked);
+    $rootScope.testResults += '\n\n';
+    if (!normalWorked || !onceWorked) {
+      console.log('finalValues: ', finalValues);
+    }
+  }
+
+  function getResult(type, totalCalls, worked) {
+    return type + ': Total calls ' + totalCalls + ' === ' + calls[type] + ' and correct values... ' + worked + ' ---> ' + (worked ? 'Success' : 'Failure') + '!';
+  }
+
+  function getIncrementer(item) {
+    return function(_new) {
+      finalValues[item] = _new;
+      calls[item]++;
+    };
+  }
+
+  function iterateTypes(fn) {
+    angular.forEach(watchTypes, function(type) {
+      fn(type, type + 'Once');
+    });
+  }
+});
